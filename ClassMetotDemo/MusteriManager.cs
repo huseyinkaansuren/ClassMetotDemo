87206fd75382df6fc851b81b2b4f0d24ede752d3
Kaan Süren <hkaansrn@gmail.com>
2021-01-19 17:55:59 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi:" + musteri.Id);
+            Console.WriteLine("Musteri Eklendi:" + musteri.Ad);
+            Console.WriteLine("Musteri Eklendi:" + musteri.Soyad);
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi:" + musteri.Id);
+            Console.WriteLine("Musteri Silindi:" + musteri.Ad);
+            Console.WriteLine("Musteri Silindi:" + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();//Müşteri objesi oluşturma
+            musteri1.Id = 1534846;
+            musteri1.Ad = "Kaan";
+            musteri1.Soyad = "Süren";
+            MusteriManager musterimanager = new MusteriManager();//Müşteri manager objesi oluşturma
+
+            musterimanager.MusteriEkleme(musteri1);//Müşteri ekleme
+            musterimanager.MusteriSilme(musteri1);//Müşteri Silme
+        }
+    }
+}
